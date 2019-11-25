93af36210fc8699aedb8155e52271c5f322c9202
Harsha Vardhan <harshatech2012@gmail.com>
2019-11-25 19:13:53 +0530

    Add ConsoleInterface interface

---
+++ src/main/java/com/harsh/bullrun/ConsoleInterface.java
@@ -0,0 +1,7 @@
+package com.harsh.bullrun;
+
+public interface ConsoleInterface {
+
+    // TODO: the javadoc should highlight to only pass those arguments that pertain to this interface
+    public void processRequest(String[] args);
+}
