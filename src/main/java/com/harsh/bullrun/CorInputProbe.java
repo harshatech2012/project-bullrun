431fb0610b711dbefd00b17751f780f5c6d6725e
Harsha Vardhan <harshatech2012@gmail.com>
2019-11-28 00:18:54 +0530

    Implement COR based Input Probe Strategy

--- src/main/java/com/harsh/bullrun/CorInputProbe.java
+++ src/main/java/com/harsh/bullrun/CorInputProbe.java
@@ -2,14 +2,21 @@ package com.harsh.bullrun;
 
 import com.google.common.collect.Sets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CorInputProbe implements InputProbingStrategy {
     private static final String name = "COR Linear Strategy";
@@ -17,12 +24,7 @@ public class CorInputProbe implements InputProbingStrategy {
     private HashProvider hashProvider;
     private RequestHandler requestHandlerChain;
 
-    private enum ProbeParameters { FILE_PATHS, ALGORITHMS, HASHES, CHECKSUMS, CHECK_POLICY, DISPLAY_POLICY }
-    private enum CheckPolicy { STRICT, NOT_STRICT }
-    private enum DisplayPolicy {
-        SHOW_CHECK,
-        OMIT_HASH // SHOW_CHECK is implicit
-    }
+    private enum ProbeParameters { FILE_PATHS, ALGORITHMS, HASHES, CHECKS, STRICT_CHECK, OMIT_HASH}
 
     {
         List<RequestHandler> handlers = new ArrayList<>();
@@ -78,7 +80,7 @@ public class CorInputProbe implements InputProbingStrategy {
                 final String optionLong = "one-to-one";
                 String[] filePaths = (String[]) request.getParameter(ProbeParameters.FILE_PATHS.name());
                 String[] algorithms = (String[]) request.getParameter(ProbeParameters.ALGORITHMS.name());
-                String[][] hashes = null;
+                String[][] hashes;
 
                 if (request.hasOption(optionLong)) {
                     if (filePaths.length != algorithms.length) {
@@ -114,15 +116,51 @@ public class CorInputProbe implements InputProbingStrategy {
         });
 
         handlers.add(new RequestHandler() {
+            // assume smallest digest size of 64 bits
+            private Pattern pattern = Pattern.compile("(\\p{XDigit}){16,}", Pattern.CASE_INSENSITIVE);
+
             @Override
             public void handle(Request request) {
                 final String optionShort = "c";
                 if (request.hasOption(optionShort)) {
                     String[] checkers = request.getOptionValues(optionShort);
 
-                    request.addParameter(ProbeParameters.CHECK_POLICY.name(), CheckPolicy.NOT_STRICT.name());
-                    request.addParameter(ProbeParameters.DISPLAY_POLICY.name(), DisplayPolicy.SHOW_CHECK.name());
-                    // todo: check whether the specified values are hashes / files
+                    Set<String> checkAgainst = new HashSet<>();
+                    BufferedReader reader = null;
+                    String line;
+                    Matcher matcher;
+                    try {
+                        for (String check : checkers) {
+                            if (this.pattern.matcher(check).matches()) {
+                                // it's a hash
+                                checkAgainst.add(check.toLowerCase());
+                            } else {
+                                // it could be a file
+                                reader = Files.newBufferedReader(
+                                        Paths.get(check).toRealPath(), StandardCharsets.UTF_8);
+                                while ((line = reader.readLine()) != null) {
+                                    matcher = this.pattern.matcher(line);
+                                    while (matcher.find()) {
+                                        // debug: check if this is properly parsing hashes
+                                        checkAgainst.add(matcher.group().toLowerCase());
+                                    }
+                                }
+                            }
+                        }
+                    } catch (IOException except) {
+                        throw new IllegalArgumentException(
+                                String.format("File not found: %s", except.getMessage()));
+                    } finally {
+                        if (reader != null) {
+                            try {
+                                reader.close();
+                            } catch (IOException error) {
+                                // Nothing to do here
+                            }
+                        }
+                    }
+
+                    request.addParameter(ProbeParameters.CHECKS.name(), checkAgainst.toArray(new String[0]));
                 }
 
                 this.getDelegate().handle(request);
@@ -139,9 +177,17 @@ public class CorInputProbe implements InputProbingStrategy {
                         throw new IllegalArgumentException("Invalid Argument: Cannot use 'strict-check' without 'c'");
                     }
 
-                    // todo: verify that number of checksums >= number of hashes
+                    String[] checks = (String[]) request.getParameter(ProbeParameters.CHECKS.name());
+                    String[][] hashes = (String[][]) request.getParameter(ProbeParameters.HASHES.name());
+                    if (checks.length < hashes.length) {
+                        throw new IllegalArgumentException(String.format(
+                                "Cannot check strictly. Hashes [%d] more than Checks [%d]",
+                                hashes.length, checks.length
+                        ));
+                    }
 
-                    request.addParameter(ProbeParameters.CHECK_POLICY.name(), CheckPolicy.STRICT.name());
+                    // fixme: replace this with bit masking parameter
+                    request.addParameter(ProbeParameters.STRICT_CHECK.name(), "");
                 }
 
                 this.getDelegate().handle(request);
@@ -158,7 +204,8 @@ public class CorInputProbe implements InputProbingStrategy {
                         throw new IllegalArgumentException("Invalid Argument: Cannot use 'omit-hash' without 'c'");
                     }
 
-                    request.addParameter(ProbeParameters.DISPLAY_POLICY.name(), DisplayPolicy.OMIT_HASH.name());
+                    // fixme: replace this with bit masking parameter
+                    request.addParameter(ProbeParameters.OMIT_HASH.name(), "");
                 }
             }
         });
@@ -166,13 +213,13 @@ public class CorInputProbe implements InputProbingStrategy {
         RequestHandler lastHandler = null;
         for (RequestHandler delegate : handlers) { // linking/chaining handlers
             if (lastHandler == null) {
+                this.requestHandlerChain = delegate;
                 lastHandler = delegate;
             } else {
                 lastHandler.setDelegate(delegate);
                 lastHandler = delegate;
             }
         }
-        this.requestHandlerChain = handlers.get(0); // first handler in the chain
     }
 
     CorInputProbe(HashProvider hashProvider) {
@@ -181,12 +228,54 @@ public class CorInputProbe implements InputProbingStrategy {
 
     @Override
     public void handleRequest (Request request) {
-        this.requestHandlerChain.handle(request); // only configures the 'request' instance
+        try {
+            this.requestHandlerChain.handle(request); // only configures the 'request' instance
 
-        // todo: add the logic for handling the request based on configured request object
-        String[] files;
-        String[] algorithms;
+            Set<String> checks = null;
+            if (request.hasParameter(ProbeParameters.CHECKS.name())) {
+                checks = new HashSet<>(Arrays.asList(
+                        (String[]) request.getParameter(ProbeParameters.CHECKS.name())
+                ));
+            }
 
+            String[][] hashes = (String[][]) request.getParameter(ProbeParameters.HASHES.name());
+            Path filePath;
+            String hashValue;
+            Set<Checksum> checksums = new HashSet<>();
+            for (String[] hashParam : hashes) {
+                filePath = Paths.get(hashParam[0]);
+                hashValue = this.hashProvider.computeHash(filePath, hashParam[1]);
+                if ((checks != null) && (request.hasParameter(ProbeParameters.STRICT_CHECK.name()) ||
+                        checks.contains(hashValue))) {
+                    checksums.add(new SimpleChecksum(
+                            filePath.getFileName().toString(), hashParam[1].toUpperCase(), hashValue,
+                            checks.remove(hashValue))
+                    );
+                } else {
+                    checksums.add(new SimpleChecksum(
+                            filePath.getFileName().toString(), hashParam[1].toUpperCase(), hashValue)
+                    );
+                }
+            }
+
+            this.render(checksums, request);
+        } catch (IOException except) {
+            // possible, despite '-f' request handler, due to TOCTTOU
+            // NOTE: this is not a check. Checking is done only in '-f' request handler
+            throw new IllegalArgumentException(String.format(
+                    "File not found: %s", except.getMessage()));
+        } catch (NoSuchAlgorithmException except) {
+            // fixme: replace System.err and printStackTrace() calls with logging
+            System.err.println("This shouldn't be reached. Else '-a' request handler is not working");
+            except.printStackTrace();
+            // this shouldn't happen
+        }
+    }
+
+    private void render(Set<Checksum> checksums, Request request) {
+        // todo: implement this
+        final byte columnWidth = 80;
+        System.out.println();
     }
 
     @Override
