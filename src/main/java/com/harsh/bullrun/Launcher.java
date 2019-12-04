5fb64d63ebad2c0a466b7964762727c705d4cbda
Harsha Vardhan <harshatech2012@gmail.com>
2019-12-04 23:21:32 +0530

    Implement logging in Launcher.java

--- src/main/java/com/harsh/bullrun/Launcher.java
+++ src/main/java/com/harsh/bullrun/Launcher.java
@@ -1,13 +1,22 @@
 package com.harsh.bullrun;
 
-import java.io.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class Launcher {
+    private static final Logger logger = LoggerFactory.getLogger(Launcher.class);
+    private static final String PROPERTIES_FILE = "application.properties";
+
     public enum LauncherModules { CHECKSUM, GPG, GUI }
 
+    // todo: mention might throw IllegalArgumentExceptions
     private void launch(LauncherModules module, String[] arguments, Properties appProperties) {
-        // todo: handle IllegalArgumentExceptions here
         switch (module) {
             case CHECKSUM:
                 ConsoleInterface ui = new ChecksumInterface(
@@ -17,29 +26,30 @@ public class Launcher {
                 ui.processRequest(arguments);
                 break;
             case GPG:
-                System.out.println("Oh! Still under development.");
+                logger.warn("GPG console invoked. Not implemented yet.");
                 // todo: implement gpg based verification
                 break;
             case GUI:
-                System.out.println("Oh! Still under development.");
+                logger.warn("GUI invoked. Not implemented yet.");
                 // todo: implement gui
                 break;
             default:
-                // This can never happen
-                break;
+                logger.error("Unmatched interface module detected - {}", module.name());
+                System.exit(-1); // exit immediately, not return
         }
     }
 
     public static void main(String[] args) {
         Launcher launcher = new Launcher();
-        LauncherModules module = null;
-        String[] arguments = null;
+        LauncherModules module;
+        String[] arguments;
         try {
             Properties appProperties = new Properties();
             InputStream inputStream = Launcher.class.getClassLoader()
-                    .getResourceAsStream("application.properties");
+                    .getResourceAsStream(Launcher.PROPERTIES_FILE);
             if (inputStream == null) {
-                throw new FileNotFoundException("application.properties file not found");
+                logger.error("Unable to find {} file. Stopping execution.", Launcher.PROPERTIES_FILE);
+                return; // only because it's main()
             }
             appProperties.load(new InputStreamReader(inputStream));
 
@@ -58,8 +68,8 @@ public class Launcher {
                     module = LauncherModules.valueOf(input.toUpperCase());
                     arguments = new String[] {"-h"};
                 } catch (IOException except) {
-                    except.printStackTrace();
-                    return;
+                    logger.error("Unable to read console input. Stopping Execution.", except);
+                    return; // only because it's main()
                 }
             } else {
                 module = LauncherModules.valueOf(args[0].toUpperCase());
@@ -73,8 +83,9 @@ public class Launcher {
 
             launcher.launch(module, arguments, appProperties);
         } catch (IllegalArgumentException | IOException except) {
-            // todo: replace with more user-friendly message
-            except.printStackTrace();
+            logger.error(except.getMessage());
         }
     }
 }
+
+// todo: register a shutdown hook and dump inputs (for later debugging and simulation)
