d8894d42be49194519c8dbe476fbfd11de690331
Harsha Vardhan <harshatech2012@gmail.com>
2020-01-15 23:36:08 +0530

    Refactor Launcher.java source code
    Refactor Launcher.java source code to better handle
    commandline arguments. Handling version and help command-line options
    from the main method was cumbersome and looked like poor design.
    Follow the ideology of "separation of concerns".
    
    Add parameter values to LauncherModules enum constants to identify
    module specific properties among other application properties.

--- src/main/java/com/harsh/bullrun/Launcher.java
+++ src/main/java/com/harsh/bullrun/Launcher.java
@@ -3,20 +3,45 @@ package com.harsh.bullrun;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
+/**
+ * Launcher is responsible for launching the application module corresponding to the
+ * command-line input. Also contains the main method for this application.
+ *
+ * @author Harsha Vardhan
+ * @since v1.0.0
+ */
 public class Launcher {
     private static final Logger logger = LoggerFactory.getLogger(Launcher.class);
     private static final String PROPERTIES_FILE = "application.properties";
 
-    public enum LauncherModules { CHECKSUM, GPG, GUI }
+    public enum LauncherModules {
+        CHECKSUM ("app.module.checksum"),
+        GPG ("app.module.gpg"),
+        GUI ("app.module.gui");
 
+        private String modulePrefix;
+
+        private LauncherModules(String modulePrefix) {
+            this.modulePrefix = modulePrefix;
+        }
+
+        String getModulePrefix() {
+            return this.modulePrefix;
+        }
+    }
+
+    private Launcher() {
+        // Default constructor
+    }
+                            
     // todo: mention might throw IllegalArgumentExceptions
-    private void launch(LauncherModules module, String[] arguments, Properties appProperties) {
+    private void launchModule(LauncherModules module, String[] arguments,
+                              Properties appProperties) {
         switch (module) {
             case CHECKSUM:
                 ConsoleInterface ui = new ChecksumInterface(
@@ -39,51 +64,98 @@ public class Launcher {
         }
     }
 
+    private void handleHelp() {
+        System.out.println("Select a module to view help:");
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Supported Modules:");
+            LauncherModules[] modules = LauncherModules.values();
+            for (LauncherModules mod : modules) {
+                System.out.println(String.format(
+                        "\t%d - %s", mod.ordinal() + 1, mod.name().toLowerCase()));
+            }
+            System.out.print("Select a module to view help: ");
+            String input = reader.readLine();
+
+//            module = LauncherModules.valueOf(input.toUpperCase());
+//            arguments = new String[] {"-h"};
+        } catch (IOException except) {
+            logger.error("Unable to read console input. Stopping Execution.", except);
+            return; // only because it's main()
+        }
+    }
+
+    private void handleVersion(Properties appProperties) {
+
+    }
+
+    /**
+     * Main entry point for launching application and its various modules.
+     *
+     * @param args command-line arguments
+     * @param appProperties properties for this application
+     */
+    private void launchApplication(String[] args, Properties appProperties) {
+        if (args.length == 0) {
+            this.handleHelp();
+        } else if (Pattern.matches("((-h)|(--help))", args[0].toLowerCase())){
+            if (args.length != 1) {
+                System.out.println("Invalid Usage: -h or --help are standalone command-line " +
+                        "options and don't take any arguments.");
+            }
+            this.handleHelp();
+        } else if (Pattern.matches("((-v)|(--version))", args[0].toLowerCase())) {
+            if (args.length != 1) {
+                System.out.println("Invalid Usage: -v or --version are standalone command-line " +
+                        "options and don't take any arguments.");
+            }
+            this.handleVersion(appProperties);
+        } else {
+            try {
+                String moduleArgument = args[0].toUpperCase();
+                LauncherModules module;
+                for (LauncherModules m: LauncherModules.values()) {
+                    if (moduleArgument.equals(m.name())) {
+                        module = LauncherModules.valueOf(moduleArgument);
+                        String[] arguments;
+                        if (args.length == 1) {
+                            arguments = new String[] {"-h"};
+                        } else {
+                            arguments = new String[args.length - 1];
+                            System.arraycopy(args, 1, arguments, 0, args.length - 1);
+                        }
+
+                        this.launchModule(module, arguments, appProperties);
+                        return; // execute this code once
+                    }
+                }
+
+                // reached if module name is invalid
+                System.out.println(String.format("Invalid Module Name: %s", args[0]));
+                this.handleHelp();
+            } catch (IllegalArgumentException except) {
+                 logger.error(except.getMessage());
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Launcher launcher = new Launcher();
-        LauncherModules module;
-        String[] arguments;
         try {
             Properties appProperties = new Properties();
             InputStream inputStream = Launcher.class.getClassLoader()
                     .getResourceAsStream(Launcher.PROPERTIES_FILE);
             if (inputStream == null) {
-                logger.error("Unable to find {} file. Stopping execution.", Launcher.PROPERTIES_FILE);
-                return; // only because it's main()
+                throw new FileNotFoundException(String.format(
+                        "Unable to find %s file. View the application log for more information.",
+                        Launcher.PROPERTIES_FILE));
             }
             appProperties.load(new InputStreamReader(inputStream));
 
-            if (args.length == 0) {
-                System.out.println("Select a module to view help:");
-                try (BufferedReader reader =
-                             new BufferedReader(new InputStreamReader(System.in))) {
-                    System.out.println("Supported Modules:");
-                    LauncherModules[] modules = LauncherModules.values();
-                    for (LauncherModules mod : modules) {
-                        System.out.println(String.format("\t%d - %s", mod.ordinal() + 1, mod.name().toLowerCase()));
-                    }
-                    System.out.print("Select a module to view help: ");
-                    String input = reader.readLine();
-
-                    module = LauncherModules.valueOf(input.toUpperCase());
-                    arguments = new String[] {"-h"};
-                } catch (IOException except) {
-                    logger.error("Unable to read console input. Stopping Execution.", except);
-                    return; // only because it's main()
-                }
-            } else {
-                module = LauncherModules.valueOf(args[0].toUpperCase());
-                if (args.length == 1) {
-                    arguments = new String[] {"-h"};
-                } else {
-                    arguments = new String[args.length - 1];
-                    System.arraycopy(args, 1, arguments, 0, args.length - 1);
-                }
-            }
-
-            launcher.launch(module, arguments, appProperties);
-        } catch (IllegalArgumentException | IOException except) {
+            launcher.launchApplication(args, appProperties);
+        } catch (IOException except) {
             logger.error(except.getMessage());
+            System.out.println(except.getMessage());
         }
     }
 }
