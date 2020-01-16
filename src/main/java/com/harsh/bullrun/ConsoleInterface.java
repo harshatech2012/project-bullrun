45a3faac4dfc18c6a2de25b2f9273387869659b2
Harsha Vardhan <harshatech2012@gmail.com>
2020-01-16 14:56:34 +0530

    Document ConsoleInterface.java

--- src/main/java/com/harsh/bullrun/ConsoleInterface.java
+++ src/main/java/com/harsh/bullrun/ConsoleInterface.java
@@ -1,7 +1,21 @@
 package com.harsh.bullrun;
 
+/**
+ * This interface is for defining console (same as command-line) based user-interface for
+ * an application module. Different modules might have different command-line options and
+ * strategies for handling them, but they should all confirm to this interface.
+ *
+ * @author Harsha Vardhan
+ * @since v1.0.0
+ */
 public interface ConsoleInterface {
 
-    // TODO: the javadoc should highlight to only pass those arguments that pertain to this interface
+    /**
+     * Processes the request corresponding to the command-line arguments. Of all the
+     * command-line arguments passed by the user, only the module relate arguments should be
+     * passed to this method.
+     *
+     * @param args command-line arguments pertaining to this module
+     */
     public void processRequest(String[] args);
 }
