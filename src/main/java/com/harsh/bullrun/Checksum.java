5aa6246ffd322f5e7f10f47a0df9ee6a1f4b98ba
Harsha Vardhan <harshatech2012@outlook.com>
2020-03-27 14:59:04 +0530

    Refactor Checksum as a marker interface

--- src/main/java/com/harsh/bullrun/Checksum.java
+++ src/main/java/com/harsh/bullrun/Checksum.java
@@ -1,55 +1,20 @@
 package com.harsh.bullrun;
 
 /**
- * This interface defines the fundamental quantities/properties for fully defining a
- * cryptographic message digest of a file.
+ * Marker interface for checksums. A checksum is a datum used to verify
+ * integrity of some data.
  */
 public interface Checksum {
 
     /**
-     * Returns the name of the file corresponding to the hash.
+     * Returns the type of checksum that this instance represents. Checksum can be generated
+     * using different types of algorithms - Cryptographic Hash Functions, Parity-Bit methods etc.
      *
-     * @return name of the file
+     * <p>The value returned by this method should be such that it could be used for
+     * identifying the checksum type.</p>
+     *
+     * @return string identifier of the checksum type. Should be user-readable and understandable.
      */
-    public String getFileName();
-
-    /**
-     * Returns the cryptographic algorithm's name used for calculating the file's digest.
-     *
-     * @return name of the cryptographic algorithm used
-     */
-    public String getAlgorithm();
-
-    /**
-     * Returns the hash/digest/checksum of the file.
-     *
-     * @return hash/digest/checksum of the file
-     */
-    public String getHashValue();
-
-    /**
-     * Returns whether the hash has been verified against a 3rd party source. Normally, the
-     * file's publisher provides checksum's, using various algorithms, for verifying file's
-     * integrity.
-     *
-     * <p>The method should return three different values, namely null, false, and true in the
-     * following cases:
-     * <ul>
-     *     <li><code>null</code> - if the calculated checksum has not been checked against any
-     *     3rd party source, like checksum from the file's official publisher</li>
-     *     <li>
-     *         But if checked against a 3rd party checksum value, then:
-     *         <ul>
-     *             <li><code>false</code> - if the checksum's don't match</li>
-     *             <li><code>true</code> - if the checksum's match</li>
-     *         </ul>
-     *     </li>
-     * </ul></p>
-     *
-     * @return null if checksum has not been checked against any 3rd party value; But if
-     * checked against a 3rd party value then true if the file's integrity has been verified, and
-     * false otherwise.
-     */
-    public Boolean isVerified();
+    public String getChecksumType();
 
 }
