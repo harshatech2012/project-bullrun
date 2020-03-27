641d8ad2c23e38ca610d4768038dab1d96a9ed49
Harsha Vardhan <harshatech2012@outlook.com>
2020-03-27 15:03:30 +0530

    Refactor SimpleChecksum to HashChecksum
    Since Checksum is now a marker interface (See commit
    SHA: 5aa6246ffd322f5e7f10f47a0df9ee6a1f4b98ba) for all
    checksums regardless of how they are calcualted. Converted
    SimpleChecksum to now represent checksums based on
    cryptographic hash functions. And consequently renamed it to
    HashChecksum. Also see this comment for more information:
    https://github.com/harshatech2012/project-bullrun/issues/4#issuecomment-604899904

--- src/main/java/com/harsh/bullrun/HashChecksum.java
+++ src/main/java/com/harsh/bullrun/HashChecksum.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A basic implementation of the {@link Checksum} interface.
+ * This interface defines the fundamental quantities/properties for fully defining a
+ * cryptographic message digest of a file.
  */
-public class SimpleChecksum implements Checksum {
-    private static final Logger logger = LoggerFactory.getLogger(SimpleChecksum.class);
+public class HashChecksum implements Checksum {
+    private static final Logger logger = LoggerFactory.getLogger(HashChecksum.class);
 
     private String fileName;
     private String algorithm;
@@ -23,7 +24,7 @@ public class SimpleChecksum implements Checksum {
      * @param verified true or false depending on whether the checksum is verified; null if the
      *                 checksum hasn't even been checked for verification of file's integrity.
      */
-    SimpleChecksum(String fileName, String algorithm, String hashValue, Boolean verified) {
+    HashChecksum(String fileName, String algorithm, String hashValue, Boolean verified) {
         this.fileName = fileName;
         this.algorithm = algorithm;
         this.hashValue = hashValue;
@@ -32,41 +33,87 @@ public class SimpleChecksum implements Checksum {
 
     /**
      * Constructor for a simple checksum. This assumes the verification status to be
-     * <code>null</code>. See {@link Checksum#isVerified()} for more information.
+     * <code>null</code>. See {@link HashChecksum#isVerified()} for more information.
      *
      * @param fileName the file whose hash is calculated
      * @param algorithm the cryptographic algorithm used to calculate the file's digest
      * @param hashValue the digest/hash of the file
      */
-    SimpleChecksum(String fileName, String algorithm, String hashValue) {
+    HashChecksum(String fileName, String algorithm, String hashValue) {
         this(fileName, algorithm, hashValue, null);
     }
 
-    @Override
+
+    /**
+     * Returns the name of the file corresponding to the hash.
+     *
+     * @return name of the file
+     */
     public String getFileName() {
         return this.fileName;
     }
 
-    @Override
+    /**
+     * Returns the cryptographic algorithm's name used for calculating the file's digest.
+     *
+     * @return name of the cryptographic algorithm used
+     */
     public String getAlgorithm() {
         return this.algorithm;
     }
 
-    @Override
+    /**
+     * Returns the hash/digest/checksum of the file.
+     *
+     * @return hash/digest/checksum of the file
+     */
     public String getHashValue() {
         return this.hashValue;
     }
 
-    @Override
+    /**
+     * Returns whether the hash has been verified against a 3rd party source. Normally, the
+     * file's publisher provides checksum's, using various algorithms, for verifying file's
+     * integrity.
+     *
+     * <p>The method should return three different values, namely null, false, and true in the
+     * following cases:
+     * <ul>
+     *     <li><code>null</code> - if the calculated checksum has not been checked against any
+     *     3rd party source, like checksum from the file's official publisher</li>
+     *     <li>
+     *         But if checked against a 3rd party checksum value, then:
+     *         <ul>
+     *             <li><code>false</code> - if the checksum's don't match</li>
+     *             <li><code>true</code> - if the checksum's match</li>
+     *         </ul>
+     *     </li>
+     * </ul></p>
+     *
+     * @return null if checksum has not been checked against any 3rd party value; But if
+     * checked against a 3rd party value then true if the file's integrity has been verified, and
+     * false otherwise.
+     */
     public Boolean isVerified() {
         return this.verified;
+    }
+
+    @Override
+    public String getChecksumType() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public String toString() {
+        // fully qualified name
+        return this.getClass().getCanonicalName();
     }
 
     @Override
     public boolean equals(Object obj) {
         boolean isEqual = false;
         if (obj != null && this.getClass().getName().equalsIgnoreCase(obj.getClass().getName())) {
-            Checksum checksum = (Checksum) obj;
+            HashChecksum checksum = (HashChecksum) obj;
             isEqual = this.getAlgorithm().equalsIgnoreCase(checksum.getAlgorithm());
             isEqual = isEqual && this.getHashValue().equalsIgnoreCase(checksum.getHashValue());
 
